0bee6984a7317521815f2874c3d914a99ee6a1ac
Wassim El Ahmar <w.elahmar@hotmail.com>
2021-07-07 03:36:52 -0400

    Added tfrecord generator

--- dataset_to_tfrecord.py
+++ dataset_to_tfrecord.py
@@ -10,6 +10,12 @@ import random
 from PIL import Image
 from object_detection.utils import dataset_util
 
+from os import listdir
+from os.path import isfile, join
+import os
+import xmltodict
+import cv2
+
 '''
 this script automatically divides dataset into training and evaluation (10% for evaluation)
 this scripts also shuffles the dataset before converting it into tfrecords
@@ -28,92 +34,78 @@ to run this script:
 $ python dataset_to_tfrecord.py 
 
 '''
-def create_example(xml_file):
+
+images_folder = "/home/wassimea/Desktop/cv/car_detection/VOCtrainval_25-May-2011/TrainVal/VOCdevkit/VOC2011/JPEGImages/"
+annotations_folder = "/home/wassimea/Desktop/cv/car_detection/VOCtrainval_25-May-2011/TrainVal/VOCdevkit/VOC2011/Annotations/"
+
+writer_train = tf.python_io.TFRecordWriter('train.record')     
+writer_test = tf.python_io.TFRecordWriter('test.record')
+
+def create_example(image_filename):
+        #image_filename = "2011_006534.jpg"
+        image_filename = "2009_005308.jpg"
         #process the xml file
-        tree = ET.parse(xml_file)
-        root = tree.getroot()
-        image_name = root.find('filename').text
-        file_name = image_name.encode('utf8')
-        size=root.find('size')
-        width = int(size[0].text)
-        height = int(size[1].text)
-        xmin = []
-        ymin = []
-        xmax = []
-        ymax = []
+        with open(annotations_folder + image_filename.replace(".jpg", ".xml")) as xml_file:
+            annotations = xmltodict.parse(xml_file.read())["annotation"]
+
+        img = cv2.imread(images_folder + image_filename)
+        height, width, channels = img.shape
+
+        xmins = []
+        ymins = []
+        xmaxs = []
+        ymaxs = []
+
         classes = []
         classes_text = []
         truncated = []
         poses = []
         difficult_obj = []
-        for member in root.findall('object'):
-           classes_text.append('Person'.encode('utf8'))
-           xmin.append(float(member[4][0].text) / width)
-           ymin.append(float(member[4][1].text) / height)
-           xmax.append(float(member[4][2].text) / width)
-           ymax.append(float(member[4][3].text) / height)
-           difficult_obj.append(0)
-           #if you have more than one classes in dataset you can change the next line
-           #to read the class from the xml file and change the class label into its 
-           #corresponding integer number, u can use next function structure
-           '''
-           def class_text_to_int(row_label):
-              if row_label == 'Person':
-                  return 1
-              if row_label == 'car':
-                  return 2
-	      and so on.....
-           '''
-           classes.append(1)   # i wrote 1 because i have only one class(person)
-           truncated.append(0)
-           poses.append('Unspecified'.encode('utf8'))
+        if isinstance(annotations['object'], list):
+            for i in range(len(annotations['object'])):
+                if annotations["object"][i]["name"] == "car":
+                    xmins.append(float(annotations["object"][i]["bndbox"]["xmin"]) / width)
+                    ymins.append(float(annotations["object"][i]["bndbox"]["ymin"]) / height)
+                    xmaxs.append(float(annotations["object"][i]["bndbox"]["xmax"]) / width)
+                    ymaxs.append(float(annotations["object"][i]["bndbox"]["ymax"]) / height)
+                    classes.append(1)
+        else:
+            if annotations["object"]["name"] == "car":
+                xmins.append(float(annotations["object"]["bndbox"]["xmin"]) / width)
+                ymins.append(float(annotations["object"]["bndbox"]["ymin"]) / height)
+                xmaxs.append(float(annotations["object"]["bndbox"]["xmax"]) / width)
+                ymaxs.append(float(annotations["object"]["bndbox"]["ymax"]) / height)
+                classes.append(1)
 
-        #read corresponding image
-        full_path = os.path.join('./JPEGImages', '{}'.format(image_name))  #provide the path of images directory
-        with tf.gfile.GFile(full_path, 'rb') as fid:
+
+        with tf.gfile.GFile(images_folder + image_filename, 'rb') as fid:
             encoded_jpg = fid.read()
-        encoded_jpg_io = io.BytesIO(encoded_jpg)
-        image = Image.open(encoded_jpg_io)
-        if image.format != 'JPEG':
-           raise ValueError('Image format not JPEG')
-        key = hashlib.sha256(encoded_jpg).hexdigest()
+
 		
         #create TFRecord Example
         example = tf.train.Example(features=tf.train.Features(feature={
             'image/height': dataset_util.int64_feature(height),
             'image/width': dataset_util.int64_feature(width),
-            'image/filename': dataset_util.bytes_feature(file_name),
-            'image/source_id': dataset_util.bytes_feature(file_name),
-            'image/key/sha256': dataset_util.bytes_feature(key.encode('utf8')),
+            'image/filename': dataset_util.bytes_feature(image_filename.encode('utf8')),
             'image/encoded': dataset_util.bytes_feature(encoded_jpg),
             'image/format': dataset_util.bytes_feature('jpeg'.encode('utf8')),
-            'image/object/bbox/xmin': dataset_util.float_list_feature(xmin),
-            'image/object/bbox/xmax': dataset_util.float_list_feature(xmax),
-            'image/object/bbox/ymin': dataset_util.float_list_feature(ymin),
-            'image/object/bbox/ymax': dataset_util.float_list_feature(ymax),
-            'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
-            'image/object/class/label': dataset_util.int64_list_feature(classes),
-            'image/object/difficult': dataset_util.int64_list_feature(difficult_obj),
-            'image/object/truncated': dataset_util.int64_list_feature(truncated),
-            'image/object/view': dataset_util.bytes_list_feature(poses),
+            'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+            'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+            'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+            'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+            'image/object/class/label': dataset_util.int64_list_feature(classes)
         }))	
         return example	
 		
 def main(_):
-    writer_train = tf.python_io.TFRecordWriter('train.record')     
-    writer_test = tf.python_io.TFRecordWriter('test.record')
-    #provide the path to annotation xml files directory
-    filename_list=tf.train.match_filenames_once("./Annotations/*.xml")
-    init = (tf.global_variables_initializer(), tf.local_variables_initializer())
-    sess=tf.Session()
-    sess.run(init)
-    list=sess.run(filename_list)
-    random.shuffle(list)   #shuffle files list
+    image_filenames = [f for f in listdir(images_folder) if isfile(join(images_folder, f))]
+    
+    random.shuffle(image_filenames)   #shuffle files list
     i=1 
     tst=0   #to count number of images for evaluation 
     trn=0   #to count number of images for training
-    for xml_file in list:
-      example = create_example(xml_file)
+    for image_filename in image_filenames:
+      example = create_example(image_filename)
       if (i%10)==0:  #each 10th file (xml and image) write it for evaluation
          writer_test.write(example.SerializeToString())
          tst=tst+1
@@ -121,7 +113,7 @@ def main(_):
          writer_train.write(example.SerializeToString())
          trn=trn+1
       i=i+1
-      print(xml_file)
+      print(image_filename)
     writer_test.close()
     writer_train.close()
     print('Successfully converted dataset to TFRecord.')
